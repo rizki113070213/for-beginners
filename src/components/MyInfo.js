d479bf3d1f77a39dfe7e2fe76d8b15d1fe5d85b1
rizki113070213 <rizki113070213@gmail.com>
2019-01-31 14:44:05 +0700

    move components into separate files

---
+++ src/components/MyInfo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MyInfo() {
+  return(
+      <div>
+        <h1>Muhammad Rizki</h1>
+        <p>This is  a paragraph about me...</p>
+      </div>
+  );
+}
+
+export default MyInfo;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,6 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
-// --------------------- Functional Components Practice ----------------------
 import React from "react";
 import ReactDOM from "react-dom";
 
-function MyInfo() {
-  return(
-    <div>
-      <h1>Muhammad Rizki</h1>
-      <p>A football lover.</p>
-      <ul>
-        <li>Raja Ampat</li>
-        <li>Paris</li>
-        <li>Munich</li>
-      </ul>
-    </div>
-  );
-}
+import MyInfo from "./components/MyInfo";
 
 ReactDOM.render(<MyInfo />, document.getElementById('root'));
