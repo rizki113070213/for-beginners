dce63c3068e401179135b3800a43295c2791b9bc
rizki113070213 <rizki113070213@gmail.com>
2019-01-31 18:05:00 +0700

    JSX to javascript and back

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
-import Header from "./components/Header"
-import MainContent from "./components/MainContent"
-import Footer from "./components/Footer"
+// import Header from "./components/Header"
+// import MainContent from "./components/MainContent"
+// import Footer from "./components/Footer"
 
 function App() {
+  const firstName = "Muhammad";
+  const lastName = "Rizki";
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+
+  if (hours < 12) {
+    timeOfDay = "morning";
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "night";
+  }
+
   return(
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
+      <h1>Hello {`${firstName} ${lastName}`}!</h1>
+      <h1>Good {timeOfDay}!</h1>
     </div>
   );
 }
