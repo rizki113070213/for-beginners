5b4bf61ec2cc79a5147632f4b937a05bc36fcb03
rizki113070213 <rizki113070213@gmail.com>
2019-02-01 10:54:58 +0700

    todo app - phase 2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from "react";
+import TodoItem from "./components/TodoItem";
 
 function App() {
-  const date = new Date();
-  const hours = date.getHours();
-  let timeOfDay;
-  const styles = {
-    fontSize: 30
-  }
-
-  if (hours < 12) {
-    timeOfDay = "morning";
-    styles.color = "#04756F"
-  } else if (hours >= 12 && hours < 17) {
-    timeOfDay = "afternoon";
-    styles.color = "#8914A3"
-  } else {
-    timeOfDay = "night";
-    styles.color = "#D90000";
-  }
-
   return(
-    <div>
-      <h1 style={styles}>Good {timeOfDay}!</h1>
+    <div className="todo-list">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
     </div>
   );
 }
