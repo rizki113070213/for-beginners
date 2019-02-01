1e97a2ed68cac4e64db93c17cb6f75a0b1c15b6e
rizki113070213 <rizki113070213@gmail.com>
2019-02-01 10:24:41 +0700

    inline styles with the style property

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from "react";
-// import Header from "./components/Header"
-// import MainContent from "./components/MainContent"
-// import Footer from "./components/Footer"
 
 function App() {
-  const firstName = "Muhammad";
-  const lastName = "Rizki";
   const date = new Date();
   const hours = date.getHours();
   let timeOfDay;
+  const styles = {
+    fontSize: 30
+  }
 
   if (hours < 12) {
     timeOfDay = "morning";
+    styles.color = "#04756F"
   } else if (hours >= 12 && hours < 17) {
     timeOfDay = "afternoon";
+    styles.color = "#8914A3"
   } else {
     timeOfDay = "night";
+    styles.color = "#D90000";
   }
 
   return(
     <div>
-      <h1>Hello {`${firstName} ${lastName}`}!</h1>
-      <h1>Good {timeOfDay}!</h1>
+      <h1 style={styles}>Good {timeOfDay}!</h1>
     </div>
   );
 }
