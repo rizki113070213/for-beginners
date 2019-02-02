4f5426e0112416cdff2539ccd3dc110b9f9b9235
rizki113070213 <rizki113070213@gmail.com>
2019-02-02 20:24:44 +0700

    state practice

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React from "react"
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      answer: "Yes"
+      name: "M Rizki",
+      age: 32
     }
   }
 
   render() {
     return(
       <div>
-        <h1>Is state important to know? {this.state.answer}</h1>
+        <h1>{this.state.name}</h1>
+        <h3>{this.state.age} years old</h3>
       </div>
     )
   }
