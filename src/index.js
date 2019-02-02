cfec66493588de2695f87c9c4eaa1c038a3e2c68
rizki113070213 <rizki113070213@gmail.com>
2019-02-02 19:30:04 +0700

    class-based components practice

--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <Header username="M Rizki" />
+        <Greeting />
+      </div>
+    )
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById('root')); 
+class Header extends React.Component {
+  render() {
+    return(
+      <header>
+        <p>Welcome, {this.props.username}!</p>
+      </header>
+    )
+  }
+}
+
+class Greeting extends React.Component {
+  render() {
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+
+    if (hours < 12) {
+      timeOfDay = "Good morning"
+    } else if (hours < 17) {
+      timeOfDay = "Good afternoon"
+    } else {
+      timeOfDay = "Good night"
+    }
+    
+    return(
+      <h1>
+        {timeOfDay} to you, sir or madam!
+      </h1>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
